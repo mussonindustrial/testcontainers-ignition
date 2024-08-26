a236def36e0930ffc067e1e5bdee36aa788e79f7
benmusson <ben.d.musson@gmail.com>
2024-08-25 21:33:00 -0400

    Add test for module inclusion.

---
+++ src/test/java/com/mussonindustrial/testcontainers/ignition/ModuleTest.java
@@ -0,0 +1,24 @@
+package com.mussonindustrial.testcontainers.ignition;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+
+@Execution(ExecutionMode.CONCURRENT)
+public class ModuleTest {
+
+    @Test
+    public void useModuleList() {
+        try (
+                IgnitionContainer ignition = new IgnitionContainer("inductiveautomation/ignition:8.1.33")
+                        .withModule(Module.OPC_UA)
+                        .withModule(Module.SQL_BRIDGE)
+        ) {
+            ignition.waitingFor(Wait.forLogMessage(".*Processing GATEWAY_MODULES_ENABLED=opc-ua,sql-bridge.*\\n", 1));
+            ignition.start();
+        }
+    }
+
+}
