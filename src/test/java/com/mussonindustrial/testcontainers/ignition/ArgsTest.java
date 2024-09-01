d3cca89d9a02c6c6e54c339b2a6d08465d54803f
benmusson <ben.d.musson@gmail.com>
2024-09-01 16:40:44 -0400

    feature: allow additional jvm/wrapper/gateway args

---
+++ src/test/java/com/mussonindustrial/testcontainers/ignition/ArgsTest.java
@@ -0,0 +1,26 @@
+package com.mussonindustrial.testcontainers.ignition;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class ArgsTest {
+
+    @Test
+    public void useModuleList() {
+        try (IgnitionContainer ignition = new IgnitionContainer("inductiveautomation/ignition:8.1.33")
+                .withAdditionalArgs("gateway.resolveHostNames=true", "gateway.useProxyForwardedHeader=true")) {
+
+            WaitAllStrategy strategy = new WaitAllStrategy();
+            strategy.withStrategy(Wait.forLogMessage(".*Collecting gateway arg: gateway.resolveHostNames=true\\n", 1))
+                    .withStrategy(
+                            Wait.forLogMessage(".*Collecting gateway arg: gateway.useProxyForwardedHeader=true\\n", 1));
+
+            ignition.waitingFor(strategy);
+            ignition.start();
+        }
+    }
+}
